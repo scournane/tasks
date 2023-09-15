8c598320233ce63f68b6dc8f1c45605d4d57aabd
scournane <samsoncournane5@gmail.com>
2023-09-14 22:19:58 -0400

    HTML CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import mountain from "./images/mountain.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -13,6 +15,35 @@ function App(): JSX.Element {
             </p>
             <p>Samson Cournane </p>
             <p>Hello World</p>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+            <h1>My name is Samson Cournane</h1>
+
+            <Container>
+                <Row>
+                    <Col>
+                        First <div id="redRectangle"></div>
+                    </Col>
+                    <Col>
+                        Second <div id="redRectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
+            <p>Things I enjoy doing</p>
+            <ul style={{ width: "7%", margin: "auto" }}>
+                <li>Hiking</li>
+                <li>Tennis</li>
+                <li>Reading</li>
+            </ul>
+            <img
+                src={mountain}
+                alt="Picture of Mountain with Clouds"
+                width={400}
+                height={500}
+            />
         </div>
     );
 }
